312c4e005eaf2d72ce00ecedeb99ed14ea52bafa
Jeff Pottruff <jeffpottruff@gmail.com>
2023-04-20 17:13:53 -0400

    add new places route. fix redirect

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import {
   BrowserRouter as Router,
   Route,
   Redirect,
+  Switch,
 } from "react-router-dom/cjs/react-router-dom";
 
 import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact>
-        <Users />
-      </Route>
-      <Redirect to="/" />
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
     </Router>
   );
 }
