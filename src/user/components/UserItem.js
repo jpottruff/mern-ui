6445475108dcc100ed02eaf60a2208ecb5bb9b24
Jeff Pottruff <jeffpottruff@gmail.com>
2023-04-25 20:13:14 -0400

    hook up users page to users list. add STUB users

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./UserItem.css";
+
+const UserItem = (props) => {
+  return <h1>user item works</h1>;
+};
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import UserList from "../components/UserList";
+
 const Users = () => {
-  return <h1>Users page works</h1>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Joe",
+      image:
+        "https://images.pexels.com/photos/895259/pexels-photo-895259.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      places: 2,
+    },
+    {
+      id: "u2",
+      name: "Jane",
+      image:
+        "https://images.pexels.com/photos/895259/pexels-photo-895259.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      places: 5,
+    },
+  ];
+  return <UserList users={USERS} />;
 };
 
 export default Users;
