31ed775a51803d07e0f2c9ee720581fd0ce46371
Jeff Pottruff <jeffpottruff@gmail.com>
2023-04-25 20:26:59 -0400

    adds user item component and hooks up to user list

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -6,16 +6,14 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
-      name: "Joe",
-      image:
-        "https://images.pexels.com/photos/895259/pexels-photo-895259.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      name: "Luna",
+      image: "https://images.pexels.com/photos/895259/pexels-photo-895259.jpeg",
       places: 2,
     },
     {
       id: "u2",
-      name: "Jane",
-      image:
-        "https://images.pexels.com/photos/895259/pexels-photo-895259.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      name: "Patti",
+      image: "https://images.pexels.com/photos/73639/pexels-photo-73639.jpeg",
       places: 5,
     },
   ];
